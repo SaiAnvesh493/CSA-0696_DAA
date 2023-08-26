184d524633b76afb7e34bd9d96d041d529f0fb1f
SaiAnvesh493 <113960097+SaiAnvesh493@users.noreply.github.com>
2023-08-26 14:37:39 +0530

    Add files via upload

---
+++ 7.selction_sort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+   int arr[100],n,i,j,position,swap;
+   printf("enter the size of array:");
+   scanf("%d",&n);
+   printf("Enter the elements in array:");
+   for(i=0;i<n;i++)
+   {
+   	scanf("%d",&arr[i]);
+   }
+   for (i = 0; i < (n - 1); i++) {
+      position = i;
+      for (j = i + 1; j < n; j++) {
+         if (arr[position] > arr[j])
+            position = j;
+      }
+      if (position != i) {
+         swap = arr[i];
+         arr[i] = arr[position];
+         arr[position] = swap;
+      }
+   }
+   for (i = 0; i < n; i++)
+      printf("%d\t", arr[i]);
+   return 0;
+}
